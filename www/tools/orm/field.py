721212e157bd10d02eb173c2ddddfdff32f371a4
John <19941222hb@gmail.com>
2015-12-23 23:21:40 +0800

    Rewrite www/tool/orm/field.py

--- www/tools/orm/field.py
+++ www/tools/orm/field.py
@@ -1,33 +1,36 @@
 #!/usr/bin/env python3
 
 class Field(object):
-	def __init__(self,name,c_type,**kw):
-		self.name=name
+	def __init__(self,c_type):
 		self.column_type=c_type
-		self.primary_key=kw.get('primary_key') if 'primary_key' in kw else False
-		self.unique_key=kw.get('unique_key') if 'unique_key' in kw else False
-		self.default=kw.get('default') if 'default' in kw else ''
-		self.Null=kw.get('null')  if 'null'in kw else False
 	def __str__(self):
-		return '<%s,%s:%s>'%(self.__class__.__name__,self.name,self.column_type)
+		return '<%s,%s>'%(self.__class__.__name__,self.column_type)
 
-class StringField(Field):
-	def __init__(self,name,*,char=False,length=255,**kw):
-		if char:
-			super().__init__(name,' char(%s) '%length,**kw)
+class String(Field):
+	def __init__(self,type_length):
+		if int(type_length)<=255:
+			super().__init__(' char(%s) '%type_length)
+		elif 65535>=int(type_length)>=255:
+			super().__init__(' varchar(%s) '%type_length)
 		else:
-			super().__init__(name,' varchar(%s) '%length,**kw)
+			super().__init__('text') 
+		
 
-class IntField(Field):
-	__type=dict({1:'tinyint',2:'smallint',4:'int',8:'bigint'})
-	def __init__(self,name,*,uint=True,length=4,**kw):
-		if uint:
-			super().__init__(name,'%s unsigned '%self.__type.get(length),**kw)
-		else:
-			super().__init__(name,' %s '%self.__type.get(length),**kw)
+class Int(Field):
 
+	def __init__(self,type_length):
+			if type_length<=1:
+				super().__init__('tinyint')
+			elif type_length<=2:
+				super().__init__('smallint')
+			elif type_length<=4:
+				super().__init__('int')
+			else:
+				super().__init__('bigint')
 
 
 if __name__=='__main__':
-	s=StringField('name',char=False,length=382)
+	s=String(25)
+	i=Int(2)
+	print(i)
 	print(s)
--- www/tools/orm/model.py
+++ www/tools/orm/model.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 
 
-
+from field import *
 class ModelMetaclass(type):
-	def __new__(cls,name,bases,attr):
+	def __new__(cls,name,bases,attrs):
+		if name=='Model':
+			return type.__new__(cls,name,bases,attrs):
+		columnsDict=dict()
+		tableName=attrs.get('__table__') or name
+                primaryKey='None'		
+		for c_name,v in attrs.items():
+			if isinstance(v,Field):
+				columnsDict[c_name]=v		
+				if v.primary_key:
+					if primaryKey:
+						raise RuntimeError('Duplicate primary key find in %s'%k)
+					primaryKey=c_name
+				
+		for column in columnsDict.keys():
+			attrs.pop(column)
+		escaped_fields=list(map(lambda f:'`%s`'%f,colunmsDict.keys()))
+		attrs['__columns__']=columnsDict
+		attrs['__table__']=tableName
+		attrs['__primary_key__']=primaryKey
+		return type.__new__(cls,name,bases,attrs)
+
+class Model(dict,metaclass=ModelMetaclass):
+	
+		
+		
+				
+
+			
+		
 
