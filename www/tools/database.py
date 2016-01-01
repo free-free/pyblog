7ab6701faa8b99aec0a8c7241b055c5609d72e74
John <19941222hb@gmail.com>
2016-01-01 23:46:09 +0800

    Rewrite database.py

--- www/tools/database.py
+++ www/tools/database.py
@@ -55,6 +55,75 @@ def execute(sql,autocommit=True):
 			raise e
 		return affectedrow			
 
+class DB(dict):
+	__connectionPool
+	def __init__(self,**kw):
+		super().__init__(**kw)
+	@classmethod
+	def createpool(cls,loop,**kw):
+		cls.__connectionPool=yield from aiomysql.create_pool(
+				host=kw.get('host','localhost'),
+				port=kw.get('port',3306),
+				user=kw.get('user','root'),
+				password=kw.get('password'),
+				db=kw.get('db','test'),
+				loop=loop
+		)
+	def table(self,tablename):
+		"""
+		@param  str tablebane
+		@return DB obj self
+		"""
+		self['table']=tableName
+		return self
+	def get(self):
+		pass
+	def select(self,sql,**kw):
+		pass
+	def update(self,sql,**kw):
+		pass
+	def insert(self,sql,**kw):
+		pass
+	def delete(self,sql,**kw):
+		pass
+	def where(self,column,operator,value):
+		pass
+	def orwhere(self,column,operator,value):
+		pass
+	def wherein(self,column,vallist):
+		pass
+	def wherenotin(self,column,vallist):
+		pass
+	def wherebetween(self,column,valrange):
+		pass
+	def wherenotbetween(self,column,valrange):
+		pass
+	def join(self,tableName,column1,operator,column2):
+		pass
+	def leftjoin(self,tableName,column2,operator,column2):
+		pass
+	def union(self):
+		pass
+	def count(self,column):
+		pass
+	def max(self,column):
+		pass
+	def min(self,column):
+		pass
+	def avg(self,column):
+		pass
+	def sum(self,column):
+		pass
+	def groupby(self,column):
+		pass
+	def orderby(self,column):
+		pass
+	def limit(self,rownum):
+		pass
+	def having(self,column,operator,value):
+		pass
+
+				
 if __name__=='__main__':
 	async def test(loop):
 		await create_pool(loop,db='pyblog',password='526114')
