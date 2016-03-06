e6542ace34917a267ea25fc51a74e81694300f1c
jell <19941222hb@gmail.com>
2016-03-06 12:57:41 +0800

    Add .gitignore

--- www/app.py
+++ www/app.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 import os,time,asyncio,json
-from datetime import datetime
-from aiohttp import web
+from   datetime import datetime
+from   aiohttp import web
 import logging;logging.basicConfig(level=logging.INFO)
-
+from   tools.log import Log
 
 def index(request):
 	return web.Response(body=b'<h1>God</h1>')
+
+
 @asyncio.coroutine
 def init(loop):
 	app=web.Application(loop=loop)
 	app.router.add_route('GET','/',index)
-	srv=yield from  loop.create_server(app.make_handler(),'121.42.169.254',80)
+	srv=yield from  loop.create_server(app.make_handler(),'127.0.0.1',8000)
 	logging.info('server started at http://121.42.169.254:80')
+	Log.info("server startd at http://127.0.0.1:8000")
 	return srv
+
 if __name__=="__main__":
 	loop=asyncio.get_event_loop()
 	loop.run_until_complete(init(loop))
