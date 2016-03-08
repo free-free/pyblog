830299032569556d5db3f696493d81fbef13ebe6
jell <19941222hb@gmail.com>
2016-03-08 22:57:24 +0800

    Rewrite config.py

--- app/tools/config.py
+++ app/tools/config.py
@@ -1,26 +1,63 @@
 # -*- coding:utf-8 -*-
-		
-class Config(dict):
-	conf=__import__('conf','app.conf')
+
+
+class DBConfigLoader(object):
+	_config_instance=None
+	
+	def __new__(cls,*args,**kw):
+		if cls._config_instance==None:
+			cls._config_instance=object.__new__(cls,*args,**kw)
+		return cls._config_instance
+	def __init__(self):
+		self._config=__import__('conf','app.conf').database
+		self._default=self._config['default']
+		self._specific=None
+	def _get_specific_config_item(self,item_name,connection):
+		if item_name in self._config['connections'][connection]:
+			return self._config['connections'][connection][item_name]
+		else:
+			raise AttributeError("dbconfig has not item '%s'"%item_name)
+	
+	def _connection(self,conn_name):
+		self._specific=conn_name
+		return self
+
+	def __getattr__(self,key):
+		if key.upper()=='CONNECTION':
+			return self._connection
+		if self._specific==None:
+			if key in self._config['connections'][self._default]:
+				return self._get_specific_config_item(key,self._default)
+			else:	
+				raise AttributeError("Can't Find config item '%s'"%key)
+		else:
+			if key in self._config['connections'][self._specific]:
+				res=self._get_specific_config_item(key,self._specific)
+				self._specific=None
+				return res
+			else:
+				raise AttributeError("Can't Find config item '%s'"%key)
+class SessionConfigLoader(object):
+	def __new__(cls,*args,**kw):
+		pass
 	def __init__(self):
 		pass
+
+class classproperty(object):
+	def __init__(self,func):
+		self._func=func
+	def __get__(self,instance,owner_class):
+		return self._func(owner_class)
+
+class Config(dict):
+	_config_loader={'database':DBConfigLoader()}
+	def __init__(self):
+		print("__init__ start")
 	def __call__(self,*args,**kw):
 		pass
-	@classmethod
-	def database(cls,*key):
-		if len(key)==1:
-			if not isinstance(key[0],str):
-				raise TypeError("config key must be str")
-			default=Config.conf.database['default']
-			if not isinstance(default,str):
-				raise TypeError
-			return Config.conf.database['connections'][default][key]
-		elif len(key)==2:
-			if not isinstance(key[0],str) or not isinstance(key[1],str):
-				raise Type("Config key must be str")
-			return Config.conf.database['connections'][key[0]][key[1]]
-		else:
-			raise Exception
+	@classproperty
+	def database(cls):
+		return cls._config_loader.get('database')
 	@classmethod
 	def session(cls,key):
 		if not instance(key,str):
