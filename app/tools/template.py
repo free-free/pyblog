a82f972a826256c1e3d0e97472583a29adc99432
jell <19941222hb@gmail.com>
2016-03-09 23:13:01 +0800

    Add Template init() method,that responsible for template config

--- app/tools/template.py
+++ app/tools/template.py
@@ -7,22 +7,25 @@ except ImportError:
     raise ImportError("Can't import jinja2 module")
 
 class Template(object):
-	def __init__(self,app,**kw):
-		options=dict(
-			autoescape=kw.get('autoescape',True),
-	    		block_start_string=kw.get('block_start_string','{%'),
-	    		block_end_string=kw.get('block_end_string','%}'),
-			variable_start_string=kw.get("variable_start_string",'{{'),
-	   	 	variable_end_string=kw.get("variable_end_string",'}}'),
-	    		auto_reload=kw.get("auto_reload",True)
-		)
-		path=os.path.join(os.path.dirname(os.path.dirname(__file__)),'templates')
-		env=Environment(loader=FileSystemLoader(path),**options)
-		app['__templating__']=env
-
+	def __init__(self,template):
+		pass	
 	@classmethod
 	def render(cls,template,**kw):
 		res={'__template__':template}
 		for k,v in kw.items():
 			res[k]=v
 		return res
+	@classmethod
+	def init(self,app,**kw):
+		options=dict(
+			autoescape=kw.get("autoescape",True),
+			block_start_string=kw.get("block_start_string",'{%'),
+			block_end_string=kw.get("block_end_string",'%}'),
+			variable_start_string=kw.get("variable_start_string",'{{'),
+			variable_end_string=kw.get("varibale_end_string",'}}'),
+			auto_reload=kw.get("auto_reload",True)
+		)
+		path=os.path.join(os.path.dirname(os.path.dirname(__file__)),'templates')
+		env=Environment(loader=FileSystemLoader(path),**options)
+		app['__templating__']=env
+	
