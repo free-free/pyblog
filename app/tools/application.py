d245c09f7b42d6a915e97c2cf4d0048160969cc2
jell <19941222hb@gmail.com>
2016-03-10 23:14:05 +0800

    Add get_server method

--- app/tools/application.py
+++ app/tools/application.py
@@ -13,16 +13,19 @@ from tools.config import Config
 from tools.database import create_pool
 logging.basicConfig(level=logging.INFO)
 class Application(web.Application):
-	def __init__(self,loop):
-		self._loop=loop
-		super(Application,self).__init__(loop=loop,middlewares=Middleware.allmiddlewares())
-	@asyncio.coroutine
+	def __init__(self):
+		self._loop=asyncio.get_event_loop()
+		super(Application,self).__init__(loop=self._loop,middlewares=Middleware.allmiddlewares())
 	def run(self,addr='127.0.0.1',port='8000'):
+		self._loop.run_until_complete(self.get_server(addr,port))
+		self._loop.run_forever()
+	@asyncio.coroutine
+	def get_server(self,addr,port):	
 		Template.init(self)
 		Route.register_route(self)
 		pool=yield from create_pool(self._loop)
 		srv=yield from self._loop.create_server(self.make_handler(),addr,port)
 		logging.info("server start at http://%s:%s"%(addr,port))
+		Log.info("server start at http://%s:%s"%(addr,port))
 		print("server start at http://%s:%s"%(addr,port))
 		return srv
-	
