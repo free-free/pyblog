0e3674c952a81f1f8d8b88d8fe9d43df3c5b09d9
jell <19941222hb@gmail.com>
2016-03-14 11:15:10 +0800

    Add session manager for different driver

--- app/tools/session.py
+++ app/tools/session.py
@@ -5,7 +5,7 @@ import time
 import os
 import json
 class Session(dict):
-	def __init__(self,session_id=None,expire=None,*args,**kw):
+	def __init__(self,session_id=None,expire=None):
 		if session_id==None:
 			self._session_id=self._generate_session_id()
 		else:
@@ -24,9 +24,17 @@ class Session(dict):
 		pass
 	def save(self):
 		pass
+	def renew(self,session_id):
+		pass
+	def set_expire(self,expire):
+		pass
 class FileSession(Session):
-	_sesion_dir='/tmp/session'
-	def __init__(self,session_id=None,expire=None,*args,**kw):
+	r'''	
+		session store in file
+		file session driver
+	'''
+	_session_dir='/tmp/session'
+	def __init__(self,session_id=None,expire=None,config=None):
 		if not os.path.exists(self._session_dir):
 			os.mkdir(self._session_dir)
 		if session_id==None:
@@ -37,10 +45,10 @@ class FileSession(Session):
 			self._session_file=os.path.join(self._session_dir,session_id)
 		if os.path.exists(self._session_file):	
 			with open(self._session_file,'r',errors='ignore',encoding='utf-8') as f:
-				self[self._session_id]=json.load(self._fp)
+				self[self._session_id]=json.load(f)
 		else:
 			self[self._session_id]={}
-		super(FileSession,self).__init__(self._session_id,expire,*args,**kw)
+		super(FileSession,self).__init__(self._session_id,expire)
 	def set(self,sname,svalue):
 		self[self._session_id][sname]=svalue
 	def get(self,sname):
@@ -48,6 +56,20 @@ class FileSession(Session):
 	def save(self):
 		with open(self._session_file,'w',errors='ignore',encoding='utf-8') as f:
 			json.dump(self[self._session_if],f)
+	def renew(self,session_id=None):
+		if session_id==None:
+			self._session_id=self._generate_session_id()
+		else:	
+			self._session_id=session_id
+		self._session_file=os.path.join(self._session_dir,self._session_id)
+		if os.path.exists(self._session_file):
+			with open(self._session_file,'r',errors='ignore',encoding='utf-8') as f:
+				self[self._session_id]=json.load(f)
+		else:
+			self[self._session_id]={}
+		return self
+	def set_expire(self,timeout):
+		self._expire=timeout
 	def __getattr__(self,k):
 		if k in self[self._session_id]:
 			return self[self._session_id].get(k)
@@ -56,13 +78,31 @@ class FileSession(Session):
 		self[self._session_id][k]=v
 
 class MongoSession(Session):
-	def __init__(self,session_id=None,expire=None,*args,**kw):
-		super(MongoSession,self).__init__(session_id,expire,*args,**kw)
+	def __init__(self,session_id=None,expire=None,config=None):
+		super(MongoSession,self).__init__(session_id,expire)
 class RedisSession(Session):
-	def __init__(self,session_id=None,expire=None,*args,**kw):
-		super(RedisSession,self).__init__(session_id,expire,*args,**kw)
+	def __init__(self,session_id=None,expire=None,config=None):
+		super(RedisSession,self).__init__(session_id,expire)
 
 class SessionManager(object):
-	def __init__(self,
-
-		
+	_drivers={}
+	_default_driver=None
+	_specific_driver=None
+	def __init__(self,session_id=None,config=None):
+		self._session_id=session_id
+	def get_mongosession_driver(self,config):
+		type(self)._drivers['mongo']=MongoSession(self._session_id,expire,config)
+		return type(self)._drivers['mongo']
+	def get_redissesssoin_driver(self,config):
+		type(self)._drivers['redis']=RedisSession(self._session_id,expire,config)
+		return type(self)._drivers['redis']
+	def get_filesession_driver(self,config):
+		type(self)._drivers['file']=FileSession(self._session_id,expire,config)
+		return type(self)._drivers['file']
+	def driver(self,driver_name,config):
+		if driver_name in self._drivers:		
+			self._specific_driver=self._drivers[driver_name]
+		else:
+			self._specific_driver= eval('self.get_%ssession_driver(%s'%(driver_name,config))
+		return self
+	
