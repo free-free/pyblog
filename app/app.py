1dcba19cc6e25d480363b90024b13a02900a18a6
jell <19941222hb@gmail.com>
2016-03-17 00:02:30 +0800

    add cookie testing code

--- app/app.py
+++ app/app.py
@@ -4,27 +4,42 @@ from tools.template import Template
 from tools.httptools import Route
 from models import *
 import hashlib
+import uuid
 @Route.get('/')
-def index():
+def index(app):
 	user=yield from  User().findone()
 	print(user.email)
-	return Template('index.html').render()
+	app.set_cookie('sl',uuid.uuid1())
+	app.set_cookie('date',uuid.uuid4())
+	#app.clear_cookie('sl')
+	return Template('index.html').render(user=user)
+@Route.get('/clc_cookie')
+def clc_cookie(app):
+	app.clear_all_cookies()
+	return 'ok'
+@Route.get('/get_cookie')
+def get_cookie(app):
+	sl=app.get_cookie('sl')
+	date=app.get_cookie('date')
+	print(app.get_all_cookies())
+	return '%s\r\n%s'%(sl,date)
 @Route.get('/user/{id}/comment/{comment}')
-def user(id,comment):
+def user(app,id,comment):
 	u=User()
 	user=yield from u.findone()
 	return Template('index.html').render(user=user)
 @Route.get('/user/register')
-def get_register():
+def get_register(app):
 	return Template("register.html").render()
 @Route.post('/user/register')
-def register(username,email,password):
+def register(app):
 	u=User()
-	u.user_name=username
+	u.user_name=app.get_argument('username')
 	md5=hashlib.md5()
+	password=app.get_argument('password')
 	md5.update(password.encode('utf-8'))
 	u.password=md5.hexdigest()
-	u.email=email
+	u.email=app.get_argument('email')
 	yield from u.save()
 	return '<h1>OK</h1>'
 if __name__=="__main__":
