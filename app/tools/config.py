c73bb1e1ffa7b983fc2cf537c9df6cdd283885a9
jell <19941222hb@gmail.com>
2016-03-19 22:21:48 +0800

    optimize DBConfigLoader code

--- app/tools/config.py
+++ app/tools/config.py
@@ -1,40 +1,35 @@
 # -*- coding:utf-8 -*-
-
+import doctest
 
 class DBConfigLoader(object):
 	_config_instance=None
-	
+	_all_connections=('mysql','mongodb')
 	def __new__(cls,*args,**kw):
 		if cls._config_instance==None:
 			cls._config_instance=object.__new__(cls,*args,**kw)
 		return cls._config_instance
 	def __init__(self):
 		self._config=__import__('conf','app.conf').database
-		self._default=self._config['default']
-		self._specific=None
-	def _get_specific_config_item(self,item_name,connection):
+		self._default_connection=self._config['default']
+		self._specific_connection=None
+	def _get_specific_connection_config_item(self,item_name,connection):
+		if connection.lower() not in self._all_connections:
+			raise AttributeError("database config has not connection %s"%connection.lower())
 		if item_name in self._config['connections'][connection]:
 			return self._config['connections'][connection][item_name]
 		else:
-			raise AttributeError("dbconfig has not item '%s'"%item_name)
-	
+			raise AttributeError("database connection has not such item '%s'"%item_name)
 	def _connection(self,conn_name):
-		self._specific=conn_name
+		self._specific_connection=conn_name.lower()
 		return self
 
 	def __getattr__(self,key):
 		if key.upper()=='CONNECTION':
 			return self._connection
-		if self._specific==None:
-			if key in self._config['connections'][self._default]:
-				return self._get_specific_config_item(key,self._default)
-			else:	
-				raise AttributeError("Can't Find config item '%s'"%key)
+		if not self._specific_connection:
+			return self._get_specific_connection_config_item(key,self._default_connection)
 		else:
-			if key in self._config['connections'][self._specific]:
-				return self._get_specific_config_item(key,self._specific)
-			else:
-				raise AttributeError("Can't find config item '%s'"%key)
+			return self._get_specific_connection_config_item(key,self._specific_connection)
 class SessionConfigLoader(object):
 	_all_drivers=('redis','file','mongo')
 	def __new__(cls,*args,**kw):
@@ -58,7 +53,7 @@ class SessionConfigLoader(object):
 	def __getattr__(self,key):
 		if key.upper()=='DRIVER':
 			return self._driver
-		if  self._specific_driver==None:
+		if  not self._specific_driver:
 			return self._get_specific_driver_config_item(key,self._default_driver)
 		else:
 			return self._get_specific_driver_config_item(key,self._specific_driver)
@@ -70,6 +65,24 @@ class classproperty(object):
 		return self._func(owner_class)
 
 class Config(dict):
+	r'''
+	>>> Config.session.session_dir
+	'/tmp/session/'
+	>>> Config.session.expire_file
+	'session_expire'
+	>>> Config.session.driver('redis').host
+	'localhost'
+	>>> Config.session.driver('redis').port
+	6379
+	>>> Config.session.driver('mongo').port
+	27017
+	>>> Config.session.driver('mongo').host
+	'localhost'
+	>>> Config.database.port
+	3306
+	>>> Config.database.connection('mongodb').port
+	27017
+	'''
 	_config_loader={'database':DBConfigLoader(),'session':SessionConfigLoader()}
 	def __init__(self):
 		print("__init__ start")
@@ -84,15 +97,20 @@ class Config(dict):
 	def __getattr__(cls,key):
 		return 'not attribute found'
 if __name__=='__main__':
-	#print(Config.session.session_dir)
-	#print(Config.session.expire_file)
-	#print(Config.session.expire)
-	#print(Config.session.driver('redis').host)
-	#print(Config.session.driver('redis').port)	
-	#print(Config.session.driver('redis').db)
-	#print(Config.session.driver('redis').expire)
-	#print(Config.session.driver('mongo').host)
-	#print(Config.session.driver('mongo').port)
-	#print(Config.session.driver('mongo').db)
-	#print(Config.session.driver('mongo').expire)
+	doctest.testmod()
+	r'''
+	print(Config.session.session_dir)
+	print(Config.session.expire_file)
+	print(Config.session.expire)
+	print(Config.session.driver('redis').host)
+	print(Config.session.driver('redis').port)	
+	print(Config.session.driver('redis').db)
+	print(Config.session.driver('redis').expire)
+	print(Config.session.driver('mongo').host)
+	print(Config.session.driver('mongo').port)
+	print(Config.session.driver('mongo').db)
+	print(Config.session.driver('mongo').expire)
 	print(Config.database.port)
+	print(Config.database.password)
+	print(Config.database.connection('mongodb').port)
+	'''
