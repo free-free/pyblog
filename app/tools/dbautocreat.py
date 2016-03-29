d205109b5057e7ffb9df7f792dd1f646e8c389fa
jell <19941222hb@gmail.com>
2016-03-29 12:11:43 +0800

    recontruct autocreate interface

--- app/tools/dbautocreat.py
+++ app/tools/dbautocreat.py
@@ -7,20 +7,6 @@ from tools.config import Config
 class AutoCreate(object):
 	def __init__(self):
 		pass
-	def _create_db(self):
-		pass
-	def _create_field_type(self):
-		pass
-	def _create_field_primary_key(self):
-		pass
-	def _create_field_unique_key(self):
-		pass
-	def _create_auto_increment(self):
-		pass
-	def _create_default(self):
-		pass
-	def _create_table(self):
-		pass
 	def run(self):
 		pass
 class MysqlAutoCreate(AutoCreate):
@@ -28,19 +14,15 @@ class MysqlAutoCreate(AutoCreate):
 		self._config=config
 		self._table=model.__table__
 		self._fields=model.__columns__
+		self._sql='create tables `%s` ('%self._table
 	@asyncio.coroutine
 	def _create_connection(self):
 		type(self)._conn=yield from aiomysql.connect(db=self._config['database'],
 								user=self._config['user'],
 								host=self._config['host'],
 								password=self._config['password'])						
-	@asyncio.coroutine
-	def _db_is_exists(self):
-		cursor=yield from self._conn.cursor()
-		yield from cursor.execute('use '+self._config['database'])
-		ret=yield from cursor.fecthall()
-	def _create_db(self,model):
-		pass	
+	def _create_field_type(self):
+		pass
 @asyncio.coroutine
 def auto_create():
 	conn=yield from aiomysql.connect(db=Config.database.database,
