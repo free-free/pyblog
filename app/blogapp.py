f73f732cb0d2f112091f86605d98fde498220f3f
jell <19941222hb@gmail.com>
2016-03-31 00:18:40 +0800

    add request handler for basic page

--- app/blogapp.py
+++ app/blogapp.py
@@ -7,7 +7,15 @@ from models import *
 def index_handler(app):
 	app.render("index.html")
 
-
+@Route.get('/articles')
+def article_list_handler(app):
+	app.render("article.html")
+@Route.get('/history')
+def article_history_handler(app):
+	app.render("list.html")
+@Route.get('/about')
+def aboutme_handler(app):
+	app.render("me.html")
 
 
 
