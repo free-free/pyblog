3e4131803db8797c8f225ad22f2fa969573763f3
jell <19941222hb@gmail.com>
2016-03-31 12:39:31 +0800

    add music handler

--- app/blogapp.py
+++ app/blogapp.py
@@ -6,7 +6,6 @@ from models import *
 @Route.get('/')
 def index_handler(app):
 	app.render("index.html")
-
 @Route.get('/articles')
 def article_list_handler(app):
 	app.render("article.html")
@@ -16,8 +15,16 @@ def article_history_handler(app):
 @Route.get('/about')
 def aboutme_handler(app):
 	app.render("me.html")
-
-
+@Route.post('/music')
+def music_handler(app):
+	d=dict();
+	d['code']=200
+	d['msg']='ok'
+	d['type']=3
+	d['data']=[
+	{'music_name':'CountrintStars','music_url':'http://7xs7oc.com1.z0.glb.clouddn.com/music%2FJason%20Chen%20-%20Counting%20Stars.mp3'},
+	]
+	return d
 
 if __name__=='__main__':
 	app=Application()
