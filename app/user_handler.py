a93153dd0ce277f4fa1d182d657b9234b0bacecf
jell <19941222hb@gmail.com>
2016-04-07 23:53:09 +0800

    add basic handler for user

--- app/user_handler.py
+++ app/user_handler.py
@@ -1,3 +1,9 @@
 #-*- coding:utf-8 -*-
 from tools.httptools import Route
 from models import User
+
+@Route.get('/{username}')
+def get_user_handler(app,username):
+	print(username)
+	app.render("admin.html")
+
