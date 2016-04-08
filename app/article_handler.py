bd7be2212ef320df9d121a0145395743db82fd78
jell <19941222hb@gmail.com>
2016-04-08 12:08:28 +0800

    add basic get_article_handler(**)

--- app/article_handler.py
+++ app/article_handler.py
@@ -1,4 +1,10 @@
 #-*- coding:utf-8 -*-
 from tools.httptools import Route
-from models import Article
+from models import Article,User
+
+@Route.get('/{username}/articles/{article_id}.html')
+def get_article_handler(app,username,article_id):
+	print(username,'==>',article_id)
+	app.render('article.html')
+	
 
