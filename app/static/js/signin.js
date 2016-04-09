d86d908ad8abd3d4f0efd4824c7df3688b615737
jell <19941222hb@gmail.com>
2016-04-09 13:39:48 +0800

    rewrite signin page

--- app/static/js/signin.js
+++ app/static/js/signin.js
@@ -1,37 +1,5 @@
 $(function(){
-		$(window).resize(function(){
-			var top=parseInt($(document).height())/2-parseInt($('.login-panel').height())/2;
-			var left=parseInt($(document).width())/2-parseInt($('.login-panel').width())/2;
-			if(parseInt(top)<60)
-			{
-				top=60;
-			}
-			$('.login-main-container').css({
-					'position':'absolute',
-					'top':top+'px',
-					'left':left+'px'
-					});	
-			$('.login-main-container').fadeIn(1200);
-		});
-		$(window).resize();
-		$('.all-input').focus(function(){
-				$(this).css({
-					'boxShadow':'0 0 5px #1bbcf2'
-				});
-				if($(this).prop('name')=='password')
-				{
-					$('.pwd-error-note').hide();
-				}
-				if($(this).prop('name')=='email'){
-					$('.email-error-note').hide();	
-				}
-		})
-		$('.all-input').blur(function(){
-				$(this).css({
-					'boxShadow':''
-				})
-				
-		});
+		
 		function create_error_note(parentname,childname,top,left,msg)
 		{
 				var note=document.createElement('div');
@@ -63,50 +31,58 @@ $(function(){
 						$(note).append(span);
 						$(parentname).append(note);			
 		}
-		$('.email-input').blur(function(){
+		$('#email').blur(function(){
 			reg=/^[\w][\w\.]+@[\w]+(\.[\w]+)+/
 			if(!reg.test($(this).val()))
 			{
 				if($('.email-error-note').length==0)
 				{
-					create_error_note('.email-box','email-error-note',45,300,'邮箱格式不正确');
+					create_error_note('.email-box','email-error-note',45,280,'邮箱格式不正确');
 				}else{
 					$('.email-error-note').show();
 				}
-				$(this).attr('id',0);
+				$(this).removeClass("active");
 				return ;
 			}
-			$(this).attr('id',1);
+			else
+			{
+				$('.email-error-note').hide();
+				$(this).addClass("active");
+			}
+			
 		});
-		$('.pwd-input').blur(function(){
+		$('#password').blur(function(){
 			var pwd=$(this).val();
 			if(pwd.length<6){
 				if($('.pwd-error-note').length==0)
 				{
-					create_error_note('.pwd-box','pwd-error-note',45,300,'密码少于六位数');
+					create_error_note('.pwd-box','pwd-error-note',45,280,'密码少于六位数');
 				}
 				else
 				{
 					$('.pwd-error-note').show();
 				}
-				$(this).attr('id',0);
+				$(this).removeClass('active');
 				return ;
-			}
-			$(this).attr('id',1);
-		});
-		$('.login-form').submit(function(){
-			$('.pwd-input').blur();
-			$('.email-input').blur();
-			if(parseInt($('.pwd-input').attr('id'))==0||parseInt($('.email-input').attr('id'))==0)
-			{
-				console.log('false');
-				return false;
 			}
 			else
 			{
-				return true;	
+				$('.pwd-error-note').hide();
+				$(this).addClass('active');
 			}
 			
+		});
+		$('.login-form').submit(function(){
+			$('#password').blur();
+			$('#email').blur();
+			if($('#password').hasClass('active')&&$('#email').hasClass('active'))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}		
 		});
 		$('.login-btn').click(function(){
 			$('.login-form').submit();
