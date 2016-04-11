772f2b1c9bfcf8062339b09b70f3499cc335daac
jell <19941222hb@gmail.com>
2016-04-11 23:15:45 +0800

    add mongo queue

--- app/tools/task_queue.py
+++ app/tools/task_queue.py
@@ -6,6 +6,12 @@ try:
 except ImportError:
 	logging.error("can't import 'redis' module")
 	exit()
+try:
+	import pymongo
+	from pymongo import MongoClient
+except ImportError:
+	logging.error("can't import 'pymongo' module")
+	exit()
 
 class RedisQueue(object):
 	_pool=None
@@ -21,10 +27,30 @@ class RedisQueue(object):
 		if not type(self)._pool:
 			type(self)._pool=redis.ConnectionPool(host=self._host,port=self._port,db=self._db)
 		self._redis_conn=redis.Redis(connection_pool=type(self._pool))
+		super(RedisQueue,self).__init__()
 	def enqueue(self,queue_name,content):
 		self._redis_conn.lpush(queue_name,content)
 		return content
 	def dequeue(self,queue_name):
 		return self._redis_conn.rpop(queue_name)
-	
+class MongoQueue(object):
+	def __init__(self,config=None):
+		if config:
+			self._host=config.get('host','localhost')
+			self._port=config.get('port',27017)
+			self._db=config.get('db','queue')
+			self._collection=config.get('collection','queue_collection')
+		else:
+			self._host='localhost'
+			self._port=27017
+			self._db='queue'
+			self._collection='queue_collection'
+		self._client=MongoClient(self._host,self._port)
+		self._mongo_conn=self._client[self._db][self._collection]
+		super(MongoQueue,self).__init__()
+	def enqueue(self,content,queue_name=None):
+		if queue_name:
+			mongo_conn=self._client[self._db][queue_name]
+		
+			
 
