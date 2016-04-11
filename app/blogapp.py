ca64615042a029b5a84a8c6840df81ab77049431
jell <19941222hb@gmail.com>
2016-04-11 09:18:35 +0800

    move handlers to corresponding handler file

--- app/blogapp.py
+++ app/blogapp.py
@@ -4,18 +4,10 @@ from tools.httptools import Route
 from models import *
 from music_handler import *
 from user_handler import *
+from article_handler import *
 @Route.get('/')
 def index_handler(app):
 	app.render("index.html")
-@Route.get('/articles')
-def get_article_list_handler(app):
-	app.render("article.html")
-@Route.get('/history')
-def get_article_history_handler(app):
-	app.render("list.html")
-@Route.get('/about')
-def get_aboutme_handler(app):
-	app.render("me.html")
 
 if __name__=='__main__':
 	app=Application()
