c573b41ce994a7f7dddf98bae2333ef4fcfa5ac8
jell <19941222hb@gmail.com>
2016-04-20 23:32:49 +0800

    change article show page

--- app/article_handler.py
+++ app/article_handler.py
@@ -2,9 +2,10 @@
 from tools.httptools import Route
 from models import Article,User
 
+@Route.get('/{username}/articles/all')
+def get_all_article_handler(app,username):
+	app.render("article.html")
 @Route.get('/{username}/articles/{article_id}.html')
-def get_article_handler(app,username,article_id):
+def get_specific_article_handler(app,username,article_id):
 	print(username,'==>',article_id)
-	app.render('article.html')
-	
-
+	app.render('article_show.html')	
