f051258e6e376c0fad703aacf21d1c255b3b77e1
jell <19941222hb@gmail.com>
2016-04-23 01:09:46 +0800

    delete http error testing code

--- app/blogapp.py
+++ app/blogapp.py
@@ -5,10 +5,10 @@ from models import *
 from music_handler import *
 from user_handler import *
 from article_handler import *
+import aiohttp
 @Route.get('/')
 def index_handler(app):
 	app.render("index.html")
-
 if __name__=='__main__':
 	app=Application()
 	app.run()
