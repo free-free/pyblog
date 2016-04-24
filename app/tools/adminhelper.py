0f50dd5819a03aed5318e08bb9749191a65b1702
jell <19941222hb@gmail.com>
2016-04-24 11:40:18 +0800

    add execute_shell_cmd()  and shell_echo()

--- app/tools/adminhelper.py
+++ app/tools/adminhelper.py
@@ -2,8 +2,32 @@
 # -*- coding:utf-8 -*-
 import argparse
 import sys
+import subprocess
+import datetime
+import shlex
 from tools.taskqueue import TaskProcessionReceiver,QueuePayloadRouter,MailExecutor
 from tools.dbautocreate import DBBuilder
+def execute_shell_cmd(cmd):
+	assert isinstance(cmd,str)
+	cmd=shlex.split(cmd)
+	sub=subprocess.Popen(cmd,stdin=subprocess.PIPE,bufsize=4096)
+	timeout=60
+	end_time=datetime.datetime.now()+datetime.timedelta(seconds=timeout)
+	while sub.poll() is None:
+		if timeout:
+			if end_time<datetime.datetime.now():
+				raise Exception("execution shell command '%s' timeout"%cmd)
+	return (sub.returncode)
+def shell_echo(string,color):
+	color_code={
+		'red':'0;31m',
+		'green':'0;32m',
+		'blue':'0;34m',
+		'brown':'0;33m'
+	}
+	cmd='echo -e "\e[{color} {string} \e[m"'.format(color=color_code.get(color.lower(),'0;30m'),string=string)
+	execute_shell_cmd(cmd)
+
 def main():
 	if len(sys.argv)==1:
 		sys.argv.append('--help')
@@ -11,7 +35,7 @@ def main():
 	parser.add_argument('--queue',help="run queue processor")
 	parser.add_argument("--port",help="queue processor listening port ",default=9999)
 	parser.add_argument("--host",help="queue processor runining host",default="localhost")
-	parser.add_argument('--dbbuild',help="build database")
+	parser.add_argument('--dbbuild',help="build database",default="start")
 	args=parser.parse_args()
 	if args.queue:
 		if args.queue.lower() =='start':
@@ -20,6 +44,6 @@ def main():
 	elif args.dbbuild:
 		if args.dbbuild.lower()=='start':
 			DBBuilder.build()
-			print("build database sucessfully!")
+			shell_echo("build database sucessfully!",'green')
 if __name__=='__main__':
 	main()
