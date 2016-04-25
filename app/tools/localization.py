cdef3dd62f04807f85315ac44a9f3867f969c582
jell <19941222hb@gmail.com>
2016-04-25 12:31:10 +0800

    implement Locale class that responsible for translating item recording to locale config

--- app/tools/localization.py
+++ app/tools/localization.py
@@ -1,6 +1,7 @@
 #-*- coding:utf-8 -*-
 import os
 import json
+from tools.config import Config
 class LocaleProxyer(dict):
 	_locale_file_dir=None
 	_locale_files_content={}
@@ -11,11 +12,11 @@ class LocaleProxyer(dict):
 	def get_locale_item(self,filename,item):
 		items=item.split('.')
 		absfilename=os.path.join(self._locale_file_dir,filename)
-		if filename not in self._locale_files_content:
+		if absfilename not in self:
 			if os.path.exists(absfilename):
 				with open(absfilename) as f:
-					type(self)._locale_files_content[absfilename]=json.load(f)
-		item_content=self._locale_files_content[absfilename]
+					self[absfilename]=json.load(f)
+		item_content=self[absfilename]
 		for item_name in items:
 			try:
 				tmp_content=item_content[item_name]
@@ -26,13 +27,21 @@ class LocaleProxyer(dict):
 		return item_content
 	
 class Locale(object):
+	_locale_dir=os.path.abspath('../locale')
+	_locale_file_suffix='.py'
 	def __init__(self,locale_proxyer=LocaleProxyer):
-		pass
+		self._locale_proxyer=locale_proxyer(os.path.join(self._locale_dir,Config.app.locale))
 	def translate(self,key):
-		pass
+		key=key.split(':')
+		locale_filename=key[0]+self._locale_file_suffix
+		locale_items=key[1]
+		return self._locale_proxyer.get_locale_item(locale_filename,locale_items)
 
 if __name__=='__main__':
-	print(LocaleProxyer('../locale/chinese').get_locale_item('message.py','login.email'))
+	l=Locale()
+	print(l.translate('message:register.password'))
+	print(l.translate('message:login.password'))
+	#print(LocaleProxyer('../locale/chinese').get_locale_item('message.py','login.email'))
 
 	
 	
