645ceb434c00b130db0db3e83eca78ee9c844892
jell <19941222hb@gmail.com>
2016-04-30 11:38:26 +0800

    Fix parameter pass error

--- app/tools/template.py
+++ app/tools/template.py
@@ -13,10 +13,10 @@ class Jinja2Template(Environment):
 	def __init__(self,path,**kw):
 		options={}
 		assert isinstance(kw.get("autoescape",True),bool)
-		assert isinstance(kw.get("block_start_string","{%"))
-		assert isinstance(kw.get("block_end_string","%}"))
-		assert isinstance(kw.get("variable_start_string","{{"))
-		assert isinstance(kw.get("variable_end_string","}}"))
+		assert isinstance(kw.get("block_start_string","{%"),str)
+		assert isinstance(kw.get("block_end_string","%}"),str)
+		assert isinstance(kw.get("variable_start_string","{{"),str)
+		assert isinstance(kw.get("variable_end_string","}}"),str)
 		assert isinstance(kw.get("auto_reload",True),bool)
 		assert isinstance(path,str)
 		options=dict(
@@ -28,7 +28,7 @@ class Jinja2Template(Environment):
 			auto_reload=kw.get("auto_reload",True)
 		)
 		self._options=options
-		super(Jinja2Template).__init__(loader=FileSystemLoader(path),**self._options)
+		super(Jinja2Template,self).__init__(loader=FileSystemLoader(path),**self._options)
 	def render(self,template,**kw):
 		assert isinstance(template,str)
 		return self.get_template(template).render(**kw)
@@ -36,7 +36,7 @@ class Jinja2Template(Environment):
 class Template(object):
 	_template_drivers={"jinja2":Jinja2Template}
 	def __init__(self,template_driver="jinja2",template_path=Config.app.template_path,**kw):
-		assert isinstance(template_driver_name,str)
+		assert isinstance(template_driver,str)
 		assert isinstance(template_path,str)
 		assert template_driver in self._template_drivers,"not support template %s"%template_driver
 		if template_path.find('.')==0:
