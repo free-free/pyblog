815eaa281969a5e21b658ce3d890d31cdaa6ee05
jell <19941222hb@gmail.com>
2016-05-02 14:48:27 +0800

    add static property class

--- app/tools/log.py
+++ app/tools/log.py
@@ -2,10 +2,25 @@
 
 from datetime import datetime
 import os
+import functools
+class staticproperty(object):
+	def __init__(self,f_get,f_set=None,f_del=None):
+		self._f_get=f_get
+		self._f_set=f_set
+		self._f_del=f_del
+	def __get__(self,obj,ownclass):
+		return self._f_get(ownclass)
+	def __set__(self,obj,value):
+		self._f_set(type(obj),value)
+	def setter(self,f_set):
+		def wrapper(*args,**kw):
+			f_set(*args,**kw)
+		self._f_set=wrapper
 class Log(object):
 	_logPath=os.path.abspath(os.path.join(os.path.dirname(__file__),'../../log'))
 	_logName='pyblog'+str(datetime.date(datetime.now()))+'.log'
 	_logFormat=""" [%s]  level:%s ==> %s \r\n"""
+	__slots__=tuple()
 	def __init__(self):
 		pass
 	@classmethod
@@ -31,13 +46,18 @@ class Log(object):
 		cls._logFormat=logformat
 	@classmethod
 	def get_logformat(cls):
-		return cks._logFormat
-	
+		return cls._logFormat
+	@staticproperty
+	def log_format(cls):
+		return cls._logFormat
+	@staticproperty
+	def log_path(cls):
+		print("getter")
+		return cls._logPath
 if __name__=='__main__':
-	sql='select * from user'.upper()
-	Log.error(sql)
-	Log.warning(sql)
-	Log.info(sql)
-
-
-		
+	r'''
+	#sql='select * from user'.upper()
+	#Log.error(sql)
+	#Log.warning(sql)
+	#Log.info(sql)
+	'''
