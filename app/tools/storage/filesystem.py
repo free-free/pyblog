80ef6940c90d5c3ad89db1542f6a42e80d8f0223
jell <19941222hb@gmail.com>
2016-05-03 14:48:56 +0800

    rename FileSystemAdapter to StorageAdapater

--- app/tools/storage/filesystem.py
+++ app/tools/storage/filesystem.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(level=logging.ERROR)
 
 
-class FileSystemAdataper(object):
+class StorageAdataper(object):
 	def __init__(self,*args,**kw):
 		pass
 	def move(self,src,dest):
