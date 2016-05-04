5a2e169904a2f0819f6341e29e7aa55490fedc63
jell <19941222hb@gmail.com>
2016-05-04 08:49:35 +0800

    import storage abstract adapter from storage_abstract module

--- app/tools/storage/qiniu_storage.py
+++ app/tools/storage/qiniu_storage.py
@@ -2,7 +2,7 @@
 import logging
 logging.basicConfig(level=logging.ERROR)
 from tools.config import Config
-from tools.storage.storage_adapter import StorageAbstractAdapter
+from tools.storage.storage_abstract import StorageAbstractAdapter
 try:
 	from qiniu import Auth,put_file,etag,urlsafe_base64_encode,BucketManager
 	import qiniu.config
