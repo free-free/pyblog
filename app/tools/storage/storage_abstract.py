193feb5b2376323cdfadb5ab504e050ac15a4ec4
jell <19941222hb@gmail.com>
2016-05-04 09:20:32 +0800

    #27,change upload_token(**) to token(**)

--- app/tools/storage/storage_abstract.py
+++ app/tools/storage/storage_abstract.py
@@ -24,10 +24,9 @@ class StorageAbstractAdapter(object):
 		pass
 	def upload(self,token,file_name,local_file):
 		pass
-	def upload_token(self,file_name,expire=3600,policy=None):
+	def token(self,file_name,expire=3600,policy=None):
 		pass
 	def download_url(self,domain,file_name,expire=3600):
 		pass
-	
 	
 
