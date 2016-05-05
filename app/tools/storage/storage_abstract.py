98e9e04019ebc6f9b3959b180356513877113a28
jell <19941222hb@gmail.com>
2016-05-05 23:36:06 +0800

    add the note for some StorageAbstractAdapter method

--- app/tools/storage/storage_abstract.py
+++ app/tools/storage/storage_abstract.py
@@ -4,9 +4,19 @@ logging.basicConfig(level=logging.ERROR)
 
 
 class StorageAbstractAdapter(object):
+	r'''
+		A abatract interface class ,that provides a common interface to access it for different third party service
+
+	'''
 	def __init__(self,*args,**kw):
+		r'''
+			The init parameter is unique for different storage service
+		'''
 		pass
 	def move(self,src,dest):
+		r'''
+			the mean of the parameter may represent different means
+		'''
 		pass
 	def copy(self,src,dest):
 		pass
@@ -23,6 +33,9 @@ class StorageAbstractAdapter(object):
 	def file_create_time(self,file_name):
 		pass
 	def put(self,token,file_name,**kw):
+		r'''
+			**kw is different for different implmentation
+		''' 
 		pass
 	def token(self,file_name,**kw):
 		pass
