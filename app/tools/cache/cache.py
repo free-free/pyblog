1da289b3b675fc2eea7e1d1f919f9716ae3ee847
jell <19941222hb@gmail.com>
2016-05-08 20:07:46 +0800

    test Cache class and fix bug

--- app/tools/cache/cache.py
+++ app/tools/cache/cache.py
@@ -2,19 +2,25 @@
 from cache_factory import CacheFactory
 from tools.config import Config
 class Cache(object):
-	def __init__(self,driver=None,config=None)
+	def __init__(self,driver=None,config=None):
 		driver=driver or Config.cache.driver_name
 		config=config or Config.cache.all
 		assert isinstance(driver,str)
 		assert isinstance(config,dict)
 		self.__default_cache_driver=self._driver(driver,config)
 		self.__current_cache_driver=self.__default_cache_driver
-	def driver(self,driver,config):
+	def driver(self,driver,config=None):
+		if not config:
+			config=Config.cache.driver(driver).all
 		self.__current_cache_driver=self._driver(driver,config)
+		return self
 	def _driver(self,driver,config):
 		return CacheFactory(driver,config)
 	def put(self,key,value,expires=None):
-		self.__current_cache_driver.put(key,value,expires)
+		if expires:
+			self.__current_cache_driver.put(key,value,expires)
+		else:
+			self.__current_cache_driver.put(key,value)
 		self.__current_cache_driver=self.__default_cache_driver
 	def get(self,key):
 		result=self.__current_cache_driver.get(key)
@@ -32,8 +38,11 @@ class Cache(object):
 		result=self.__current_cache_driver.decrement(key,delta)
 		self.__current_cache_driver=self.__default_cache_driver
 		return result
-	def update(self,key,value,expires=None)
-		result=self.__current_cache_driver.update(key,value,expires)
+	def update(self,key,value,expires=None):
+		if expires:
+			result=self.__current_cache_driver.update(key,value,expires)
+		else:
+			result=self.__current_cache_driver.update(key,value)
 		self.__current_cache_driver=self.__default_cache_driver
 	def exists(self,key):
 		result=self.__current_cache_driver.exists(key)
@@ -45,4 +54,9 @@ class Cache(object):
 		return result
 	
 		
-
+if __name__=='__main__':
+	pass
+	#cache=Cache()
+	#cache.driver("memcache").put("user:1",{"name":"xxx"})
+	#print(cache.driver("memcache").get_delete("user:1"))
+	
