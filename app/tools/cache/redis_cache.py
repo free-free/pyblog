bebf39c5a219a38158300c190eb4c9498b7e07ab
jell <19941222hb@gmail.com>
2016-05-09 11:39:58 +0800

    update parameter list in RedisCache's methods

--- app/tools/cache/redis_cache.py
+++ app/tools/cache/redis_cache.py
@@ -33,26 +33,26 @@ class RedisCacheClient(object):
 			pipe=self._connection.pipeline()
 			pipe.hset(self.__key_type_hash,key,1)
 			pipe.set(key,value)
-			if expires:
+			if expires>0:
 				pipe.expire(key,expires)
 			pipe.execute()
 		elif isinstance(value,dict):
 			pipe=self._connection.pipeline()
 			pipe.hset(self.__key_type_hash,key,2)
 			pipe.hmset(key,value)
-			if expires:
+			if expires>0:
 				pipe.expire(key,expires)
 			pipe.execute()
 		elif isinstance(value,(list,tuple)):
 			pipe=self._connection.pipeline()
 			pipe.hset(self.__key_type_hash,key,3)
 			pipe.lpush(key,*value)
-			if expires:
+			if expires>0:
 				pipe.expire(key,expires)
 			pipe.execute()
 	def get(self,key,key_prefix):
+		key_type=self.exists(key,key_prefix)
 		key=key_prefix+key
-		key_type=self.exists(key)
 		if key_type=="string":
 			return self._connection.get(key) or ""
 		elif key_type=="hash":
@@ -62,31 +62,30 @@ class RedisCacheClient(object):
 		else:
 			return None
 	def delete(self,key,key_prefix):
-		key=key_prefix+key
-		key_type=self.exists(key)
+		key_type=self.exists(key,key_prefix)
 		result=""
 		if key_type=="string":
-			result=self._connection.delete(key)
+			result=self._connection.delete(key_prefix+key)
 		elif key_type=="hash":
-			result=self._connection.hdel(key,*self._connection.hkeys(key))
+			result=self._connection.hdel(key,*self._connection.hkeys(key_prefix+key))
 		elif key_type=="list":
-			length=self._connection.llen(key)
+			length=self._connection.llen(key_prefix+key)
 			if length:
-				result=self._connection.ltrim(key,length+1,length+1)
+				result=self._connection.ltrim(key_prefix+key,length+1,length+1)
 		else:
 			result=None
-		self.delete_key(key)
+		self.delete_key(key,key_prefix)
 		return result
 	def inc(self,key,delta,key_prefix):
+		key_type=self.exists(key,key_prefix)
 		key=key_prefix+key
-		key_type=self.exists(key)
 		if key_type=="string":
 			return self._connection.incrby(key,delta)
 		else:
 			raise TypeError("can't  increment  '%s'"%key)
 	def dec(self,key,delta,key_prefix):
+		key_type=self.exists(key,key_prefix)
 		key=key_prefix+key
-		key_type=self.exists(key)
 		if key_type=="string":
 			return self._connection.incrby(key,delta)
 		else:
@@ -106,24 +105,24 @@ class RedisCache(CacheAbstractDriver):
 		return str(self.__client)
 	def __repr__(self):
 		return str(self.__client)
-	def put(self,key,content,expires=None):
-		self.__client.set(key,content,expires)
-	def delete(self,key):
-		return self.__client.delete(key)
-	def get(self,key):
-		return self.__client.get(key)
-	def get_delete(self,key):
-		re=self.__client.get(key)
-		self.__client.delete(key)
+	def put(self,key,value,expires=0,key_prefix=""):
+		self.__client.set(key,value,expires,key_prefix)
+	def delete(self,key,key_prefix=""):
+		return self.__client.delete(key,key_prefix)
+	def get(self,key,key_prefix=""):
+		return self.__client.get(key,key_prefix)
+	def get_delete(self,key,key_prefix=""):
+		re=self.__client.get(key,key_prefix)
+		self.__client.delete(key,key_prefix)
 		return re
-	def exists(self,key):
-		return self.__client.exists(key)
-	def increment(self,key,num=None):
-		return self.__client.inc(key,num)
-	def decrement(self,key,num):
-		return self.__client.dec(key,num)
-	def update(self,key,expires):
-		return self.__client.add(key,content,expires)
+	def exists(self,key,key_prefix=""):
+		return self.__client.exists(key,key_prefix)
+	def increment(self,key,delta=1,key_prefix=""):
+		return self.__client.inc(key,delta,key_prefix)
+	def decrement(self,key,delta=1,key_prefix=""):
+		return self.__client.dec(key,delta,key_prefix)
+	def update(self,key,value,expires=0,key_prefix=""):
+		return self.__client.set(key,value,expires,key_prefix)
 	
 	
 
@@ -131,13 +130,16 @@ class RedisCache(CacheAbstractDriver):
 
 if __name__=='__main__':
 	pass
-	#re=RedisCache(1,host='localhost',port=6379)
-	#re.put("user:1","hello")
-	#re.put("user:2",[21,32,32,121,42])
-	#re.put("user:3",{"name":"huangbiao","age":22,"email":"19941222hb@gmail.com"})
-	#re.delete("user:3")
+	r'''
+	#re=RedisCache('localhost',6379,1)
+	#re.put("1","hello",key_prefix="user:name:")
+	#re.put("2",[21,32,32,121,42],key_prefix="user:list:")
+	#re.put("3",{"name":"huangbiao","age":22,"email":"19941222hb@gmail.com"},key_prefix="user:info:")
+	#print(re.get_delete("3",key_prefix="user:info:"))
+	#print(re.get_delete("2",key_prefix="user:list:"))
+	#print(re.get_delete("1",key_prefix="user:name:"))
 	#print(re.get("user:3"))
-	
+	'''
 
 
 	
