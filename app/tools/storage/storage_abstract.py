62b4e7c22f178af0bcdfcd106c479be2f151c8bc
jell <19941222hb@gmail.com>
2016-05-12 14:31:41 +0800

    rewrite storage abstract class

--- app/tools/storage/storage_abstract.py
+++ app/tools/storage/storage_abstract.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(level=logging.ERROR)
 
 
-class StorageAbstractAdapter(object):
+class StorageAbstractAdapter:
 	r'''
 		A abatract interface class ,that provides a common interface to access it for different third party service
 
@@ -17,31 +17,31 @@ class StorageAbstractAdapter(object):
 		r'''
 			the mean of the parameter may represent different means
 		'''
-		pass
+		raise NotImplementedError
 	def copy(self,src,dest):
-		pass
+		raise NotImplementedError
 	def delete(self,src):
-		pass
+		raise NotImplementedError
 	def file_size(self,file_name):
-		pass
+		raise NotImplementedError
 	def file_hash(self,file_name):
-		pass
+		raise NotImplementedError
 	def file_info(self,file_name):
-		pass
+		raise NotImplementedError
 	def file_mime(self,file_name):
-		pass
+		raise NotImplementedError
 	def file_create_time(self,file_name):
-		pass
+		raise NotImplementedError
 	def put(self,token,file_name,**kw):
 		r'''
 			**kw is different for different implmentation
 		''' 
-		pass
+		raise NotImplementedError
 	def token(self,file_name,**kw):
-		pass
+		raise NotImplementedError
 	def get(self,file_name,**kw):
-		pass
+		raise NotImplementedError
 	def get_url(self,file_name,**kw):
-		pass
+		raise NotImplementedError
 	
 
