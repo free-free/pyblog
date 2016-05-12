5b08c7cc07591be954201e11fa22cbd8f077d67b
jell <19941222hb@gmail.com>
2016-05-12 14:38:03 +0800

    add comments for each method

--- app/tools/storage/storage_abstract.py
+++ app/tools/storage/storage_abstract.py
@@ -4,44 +4,80 @@ logging.basicConfig(level=logging.ERROR)
 
 
 class StorageAbstractAdapter:
-	r'''
+	r"""
 		A abatract interface class ,that provides a common interface to access it for different third party service
 
-	'''
+	"""
 	def __init__(self,*args,**kw):
-		r'''
+		r"""
 			The init parameter is unique for different storage service
-		'''
+		"""
 		pass
 	def move(self,src,dest):
-		r'''
-			the mean of the parameter may represent different means
-		'''
+		r"""
+		    @move a file 
+	            @parameters:
+			src:source file name,must be 'str' type
+		        dest:destination file name,must be 'str' type
+		"""
 		raise NotImplementedError
-	def copy(self,src,dest):
+	def copy(self,src,dest):	
+		r"""
+		    @copy a file
+		    @parameters:
+			src:source file name,'str' type
+			dest:destination file name,'str' type
+		"""
 		raise NotImplementedError
 	def delete(self,src):
+		r"""
+		    @delete a file 
+		"""
 		raise NotImplementedError
 	def file_size(self,file_name):
+		r"""
+		    @get file size information
+		"""
 		raise NotImplementedError
 	def file_hash(self,file_name):
+		r"""
+		    @get file hash value
+		"""
 		raise NotImplementedError
 	def file_info(self,file_name):
+		r"""
+		    @get file all information
+		"""
 		raise NotImplementedError
 	def file_mime(self,file_name):
+		r"""
+		    @get file mime type information
+		"""
 		raise NotImplementedError
 	def file_create_time(self,file_name):
+		r"""
+		    @get file create time
+		"""
 		raise NotImplementedError
 	def put(self,token,file_name,**kw):
 		r'''
-			**kw is different for different implmentation
+		    @update a file
 		''' 
 		raise NotImplementedError
 	def token(self,file_name,**kw):
+		r"""
+		    @generate upload token 
+		"""
 		raise NotImplementedError
 	def get(self,file_name,**kw):
+		r"""
+		    @download file 
+		"""
 		raise NotImplementedError
 	def get_url(self,file_name,**kw):
+		r"""
+		    @generate download file url
+		"""
 		raise NotImplementedError
 	
 
