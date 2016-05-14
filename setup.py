d7ff3fa0c5b248841826750557faf3f778725d59
jell <19941222hb@gmail.com>
2016-05-14 18:50:32 +0800

    add setup code

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3.5
+import os
+import sys
+from setuptools import find_packages,setup
+
+setup(
+	name="pyblog",
+	version="1.0",
+	author="whoami",
+	author_email="19941222hb@gmail.com",
+	description="An asynchronous web framework that base on aiohttp",
+	license="MIT",
+	packages=find_packages(),
+	include_package_data=True,
+	install_requires=[line for line in open('requirements.txt','r').read()]
+)
