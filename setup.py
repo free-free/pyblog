9da5b1968b2855924e37ce6375ffc664e5d2f1bc
jell <19941222hb@gmail.com>
2016-05-14 19:44:15 +0800

    add install_require

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.5
 import os
 import sys
+import re
 from setuptools import find_packages,setup
 
 setup(
@@ -12,5 +13,5 @@ setup(
 	license="MIT",
 	packages=find_packages(),
 	include_package_data=True,
-	install_requires=[line for line in open('requirements.txt','r').read()]
+	install_requires=re.split(r'\r*\n+',open("requirements.txt",'r').read())
 )
